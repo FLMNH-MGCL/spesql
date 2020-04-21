4eebb76a92f0e973bac57161f828c156f2ae6856
Aaron Leopold <aaronleopold1221@gmail.com>
2020-04-21 11:53:48 -0400

    converting to yarn from npm

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -2,7 +2,11 @@
 //'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
 
 module.exports = {
-    db: {
-        uri: '', //place the URI of your mongo database here.
-    }
+  mysqlCredentials: {
+    host: "",
+    port: "",
+    user: "",
+    password: "",
+    database: "",
+  },
 };
